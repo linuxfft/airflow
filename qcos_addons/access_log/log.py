8ba24d18d94f37c1ac67fea839d4201dcb2b82f0
Cosine <cooosine@outlook.com>
2021-12-29 13:01:07 +0800

    [FIX]: 修复access_log

--- qcos_addons/access_log/log.py
+++ qcos_addons/access_log/log.py
@@ -1,15 +1,27 @@
+import json
+from typing import Callable, TypeVar, cast
+
 from .constants import CUSTOM_LOG_FORMAT, CUSTOM_EVENT_NAME_MAP, CUSTOM_PAGE_NAME_MAP
 from datetime import datetime
 import logging
-from flask_login import current_user  # noqa: F401
 from airflow.settings import TIMEZONE
+from airflow.utils.session import create_session
+import functools
+
+T = TypeVar("T", bound=Callable)
 
 _logger = logging.getLogger(__name__)
 
 
 def access_log(event, page, msg):
-    def decorator(func):
-        def wrapped(*args, **kwargs):
+    def decorator(func: T) -> T:
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            _logger.info(repr(args))
+            _logger.info(repr(kwargs))
+
+            ret = func(*args, **kwargs)
+            from flask_login import current_user  # noqa: F401
             full_msg = CUSTOM_LOG_FORMAT.format(
                 datetime.now(tz=TIMEZONE).strftime("%Y-%m-%d %H:%M:%S"),
                 current_user if current_user and current_user.is_active else 'anonymous',
@@ -17,6 +29,8 @@ def access_log(event, page, msg):
                 CUSTOM_EVENT_NAME_MAP[event], CUSTOM_PAGE_NAME_MAP[page], msg
             )
             _logger.info(full_msg)
-            return func(*args, **kwargs)
-        return wrapped
+            return ret
+
+        return cast(T, wrapper)
+
     return decorator
